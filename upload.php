eeff212d488f64fcec17f98653238c1b703e666e
Mustafa Yılmaz <mustafayilmaz.pro@gmail.com>
2018-12-05 17:00:55 +0300

    son değişiklikle yapıldı. klasör oluşturma şuanlık yok

--- upload.php
+++ upload.php
@@ -1,106 +1,124 @@
 <?php 
-/*
-Mustafa Yılmaz
-*/
-class MUpload
+//  Array
+// (
+// [resim] => Array
+// (
+// [name] => site taslak.fonksiyon.png
+// [type] => image/png
+// [tmp_name] => Z:\xampp\tmp\phpAAC1.tmp
+// [error] => 0
+// [size] => 8564
+// )
+// )
+
+class Upload
 {
-	// $_FILES["name"]
-	private $file; 
-	// yüklenen dosya adı
-	public $yuklenen;
-	// yükleme esnasında yakalanacak hatalar
-	public $hata;
-	// dosya yüklemesinde yeni ada eklenecek olan ön ek
-	public $dosya_on_ek="";
+	/**
+	 * [$dosya -> $_FILES["dosya"]]
+	 */
+	private $dosya;
+	/**
+	 * [$yol -> dosyanın yüklenecek olduğu klasör ismi]
+	 */
+	private $yol;
+	/**
+	 * [$dosyaAdi yüklenen dosyanın adı]
+	 */
+	private $dosyaAdi;
 
-	public function __construct($param)
-	{
-		$this->file=$param;
+	/**
+	 * [__construct -> $_FILES["dosya"]] adını $dosya özelliğine atar.
+	 */
+	function __construct($file){
+		$this->dosya=$file;
 	}
-	// sadece dosya adını alan metod
-	// resim.png ise
-	// return resim
-	private function ad(){
-		$ad_uzanti = explode(".",$this->file["name"]);
-		$sadece_ad = "";
-		for ($i=0; $i<count($ad_uzanti)-1; $i++) { 
-			$sadece_ad = $sadece_ad ."". $ad_uzanti[$i] ."_";
+	/**
+	 * [sadeceAd -> dosya ad ve uzantısından uzantı harici bütün isimleri alır.]
+	 * @return [type] [sadece ad]
+	 */
+	public function sadeceAd(){
+		$adUzanti = explode(".", $this->dosya["name"]);
+		/**
+		 * site taslak.fonksiyon.png
+		 * [0]site taslak
+		 * [1]fonksiyon
+		 * [2]png
+		 */
+		$sadeceAd="";
+		for ($i=0; $i<count($adUzanti)-1; $i++) { 
+			$sadeceAd .= $adUzanti[$i];
 		}
-		return $sadece_ad;
+		return $sadeceAd;
 	}
-	// sadece dosya uzantısını alan metod
-	// resim.png ise
-	// return png	
-	private function uzanti()
+	/**
+	 * [sadeceUzanti -> dosya ad ve uzantısından sadece uzanti değerini alır.]
+	 * @return [type] [description]
+	 */
+	public function sadeceUzanti(){
+		$adUzanti = explode(".", $this->dosya["name"]);
+		/**
+		 * site taslak.fonksiyon.png
+		 * [0]site taslak
+		 * [1]fonksiyon
+		 * [2]png
+		 */
+		$sadeceUzanti= $adUzanti[count($adUzanti)-1];
+		return $sadeceUzanti;
+	}
+	/**
+	 * [yeniAdOlustur -> dosya adında büyük A-Z küçük a-z ve rakam dışında bulunan tüm karakterleri temizleyip boşlukları - yapar. ve dosyanın adına benzersiz bir ıd değeri atar.]
+	 * @param  [type] $text [metodun aldığı parametre]
+	 * @return [type]       [$yeni temizlenmiş olan değer]
+	 */
+	function yeniAdOlustur($text)
 	{
-		$ad_uzanti = explode(".",$this->file["name"]);
-		$sadece_uzanti = $ad_uzanti[count($ad_uzanti)-1];
-		return $sadece_uzanti;
+		$tr = array('Ç', 'Ş', 'Ğ', 'Ü', 'İ', 'Ö', 'ç', 'ş', 'ğ', 'ü', 'ö', 'ı', '+', '#');
+		$ing = array('c', 's', 'g', 'u', 'i', 'o', 'c', 's', 'g', 'u', 'o', 'i', '', '');
+		$text = strtolower(str_replace($tr, $ing, $text));
+		$text = preg_replace("@[^A-Za-z0-9\-_\.\+]@i", ' ', $text);
+		$text = trim(preg_replace('/\s+/', ' ', $text));
+		$text = str_replace(' ', '-', $text);
+		$text .= "_". $this->uniqIdOlustur();
+		return $text;
 	}
-	// rastgele benzersiz kimlik üreten metod
-	private function rastgeleDeger(){
-		$zaman = date("Ymd");
-		$kimlik = uniqid();
-		$sonuc = $kimlik."".$zaman;
-		return $sonuc;
+	/**
+	 * [uniqIdOlustur -> 13 karakterli benzersiz bir id değeri oluşturur.]
+	 * @return [type] [uniqid]
+	 */
+	function uniqIdOlustur(){
+		return uniqid();
 	}
-	
-	// $param da bulunan türkçe karakterleri ve A-Z,a-z,0-9,-ve _ dışındaki karakterleri temizleyip küçük harflere döndüren metod
-	private function temizle($param)
+	/**
+	 * [dosyaAdi yüklenen dosyanın adını verir.]
+	 * @return [type] [yeni yüklenen dosyanın en son adı]
+	 */
+	public function dosyaAdi()
 	{
-		$eski = ["ç","Ç","ı","İ","ğ","Ğ","ş","Ş","ü","Ü","ö","Ö"];
-		$yeni = ["c","C","i","I","g","G","s","S","u","U","o","O"];
-		$param = str_replace($eski, $yeni, $param);
-		$param = preg_replace("@[^A-Za-z0-9\-_]+@i","",$param);
-		$param = strtolower($param);
-		return $param;
+		return $this->dosyaAdi;
 	}
-	// dosya yüklemesini yapan metod
-	// eğer yol yok ise oluşturur.
-	public function yukle($yol="")
-	{
-		$ad = $this->ad();
-		$uzanti = $this->uzanti();
-		$yeni_ad = $this->temizle($ad);
-		if ($this->dosya_on_ek=="") {
-			$son_ad= $yeni_ad ."". $this->rastgeleDeger() .".". $uzanti;
+	/**
+	 * [yukle ->eğer yol boş ise bulunduğu klasöre eğer yol boş değilse belirtilen klasöre yeni ad verilen dosyayı yükler ve sonucunu true yada false ile döndürür.]
+	 * @param  string $yol [dosyanın yükleneceği klasör]
+	 * @return [type]      [true/false]
+	 */
+	public function yukle($yol=""){
+		$ad = $this->sadeceAd();
+		$uzanti = $this->sadeceUzanti();
+		$yAd =  $this->yeniAdOlustur($ad);
+		$dosyaAdi = $yAd.".".$uzanti;
+		
+		if ($yol=="") {
+			$yuklenecekDosya = $dosyaAdi;
 		}else{
-			$son_ad= $this->dosya_on_ek."_".$yeni_ad ."". $this->rastgeleDeger() .".". $uzanti;
-		}
-		if ( $yol!="") {
-
-			function mkdir_r($dirName, $izin=0644,$boo="false"){
-				$dirs = explode('/', $dirName);
-				$dir='';
-				$hata =true;
-				foreach ($dirs as $part) {
-					$dir.=$part.'/';
-					if (!is_dir($dir) && strlen($dir)>0)
-						if(!mkdir($dir, $izin,$boo)){
-							$hata = false;
-						}
-				}
-				return $hata;
-			}
-
-			if(!mkdir_r($yol,0644,true)){
-				$this->hata="Dizin Oluşturulamadı.";
-				return false;
-				exit();
-			}else{
-				$son = $yol ."/". $son_ad;
-			}
-		}else{
-			$son = $son_ad;
+			$yuklenecekDosya = $yol ."/". $dosyaAdi;
 		}
 
-		$yukle = move_uploaded_file($this->file["tmp_name"], $son);
-		if ( $yukle ) {
-			$this->yuklenen = $son_ad;
+		$yuklenenDosya = move_uploaded_file($this->dosya["tmp_name"], $yuklenecekDosya);
+		if ($yuklenenDosya) {
+			$this->dosyaAdi=$dosyaAdi;
 			return true;
 		}else{
 			return false;
 		}
-
 	}
 }
